1569e3ade8118b3206cc812adafbcd4a58e5ae19
Pratik Kulkarni <pnkulkarni@proton.me>
2026-04-19 14:41:55 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -9,17 +9,24 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+func handleConnection(conn net.Conn) {
+	conn.Write([]byte("+PONG\r\n"))
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
 
-	conn.Write([]byte("+PONG\r\n"))
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
 }
